6b1c2a22328d2451e23a4a42e3339adb23b18730
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-11 17:23:35 +0300

    Used the custom keymaps to help control the player movement

--- Scripts/manage_player.gd
+++ Scripts/manage_player.gd
@@ -33,3 +33,20 @@ func _process(delta: float) -> void:
 			pc_node.play("running");
 			var forward = global_transform.basis.z;
 			move_and_slide(forward*speed, Vector3.UP);
+
+
+func _input(event: InputEvent) -> void:
+	if(Input.is_action_pressed("move_fwd")):
+		current_state = WALK;
+	elif(Input.is_action_pressed("move_back")):
+		current_state = WALK_REVERSE;
+	else:
+		current_state = IDLE;
+		
+	if(Input.is_action_pressed("turn_left")):
+		rotate_y(rotation_increment);
+	elif(Input.is_action_pressed("turn_right")):
+		rotate_y(-rotation_increment);
+	
+	if(Input.is_key_pressed(KEY_SHIFT)):
+		current_state = RUN;
