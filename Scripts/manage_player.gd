a9cda5c65f23d738d0abc0d9651a9373b9539912
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-11 17:10:57 +0300

    Added a FSM to the process function

--- Scripts/manage_player.gd
+++ Scripts/manage_player.gd
@@ -12,3 +12,15 @@ func _ready() -> void:
 	pc_node.get_animation("idle").loop = true;
 	pc_node.get_animation("walking").loop = true;
 	pc_node.get_animation("running").loop = true;
+
+
+func _process(delta: float) -> void:
+	match current_state:
+		IDLE:
+			pass
+		WALK:
+			pass
+		WALK_REVERSE:
+			pass
+		RUN: 
+			pass
