928652839a23cb86a2a8e7d3537b457a48e48273
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-21 22:35:11 +0300

    Added some code in the ready function

--- Scripts/InventorySystem.gd
+++ Scripts/InventorySystem.gd
@@ -10,3 +10,13 @@ onready var inventory_text=get_node(path+"inventory_text");
 onready var inventory_image=get_node(path+"inventory_image");
 onready var inventory_description=get_node(path+"inventory_description");
 onready var inventory_panel=get_node(path+"inventory_panel");
+
+
+func _ready() -> void:
+	display_ui(false);
+	player_inventory=[];
+	var new_item = Item.new(Item.item_type.GOLD);
+	player_inventory.push_back(new_item);
+	new_item=Item.new(Item.item_type.MEAT);
+	player_inventory.push_back(new_item);
+	player_inventory[1].nb=300;
